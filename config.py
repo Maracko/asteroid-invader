3c0a11c4d75c3a566a6cc448f31c8e87b4028a02
Mario Petričko <petricko.mario@gmail.com>
2020-07-07 21:49:52 +0200

    gitignore, config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+WIN_WIDTH = 800
+WIN_HEIGHT = 800
+SHIP_WIDTH = 56
+SHIP_HEIGHT = 60
+ASTEROID_WIDTH = 55
+ASTEROID_HEIGHT = 56
+BULLET_WIDTH = 25
+BULLET_HEIGHT = 30
+
+BULLET_DELAY = 0.5
+HIT_DELAY = 1 #max hits per second
+BULLET_SPEED = 7 # speed of bullet trajectory
+ASTEROID_SPEED = 5 # controls the move speed of asteroids
+
+score = 0
+high_score = 0
+health = 100
